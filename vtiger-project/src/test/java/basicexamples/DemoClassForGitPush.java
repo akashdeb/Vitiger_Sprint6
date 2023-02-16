61603cc6a44ff770faaa9aec65758aa38f762b68
akashdeb <akashdeb87@gmail.com>
2023-02-16 14:27:49 +0530

    Change from TE for conflict

--- vtiger-project/src/test/java/basicexamples/DemoClassForGitPush.java
+++ vtiger-project/src/test/java/basicexamples/DemoClassForGitPush.java
@@ -5,10 +5,9 @@ import org.testng.annotations.Test;
 public class DemoClassForGitPush {
 	
 	@Test
-	public void demoGitConflictTest() {
 	public void demoTest() {
 		
-		System.out.println("Demo push Git conflict changes from TE2");		
+		System.out.println("Demo push Git conflict changes from TE2 change 2");		
 	}
 
 }

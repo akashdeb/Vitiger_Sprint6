1b5d57286453a7d7799b02df21e6be5619cacfc3
akashdeb <akashdeb87@gmail.com>
2023-02-16 14:01:46 +0530

    Added Demo Class for push

---
+++ vtiger-project/src/test/java/basicexamples/DemoClassForGitPush.java
@@ -0,0 +1,14 @@
+package basicexamples;
+
+import org.testng.annotations.Test;
+
+public class DemoClassForGitPush {
+	
+	@Test
+	public void demoTest() {
+		
+		System.out.println("Demo class for git push");
+		
+	}
+
+}
